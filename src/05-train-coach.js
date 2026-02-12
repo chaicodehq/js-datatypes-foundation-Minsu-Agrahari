28b91283a6ebb9d220218505c647cff61e595446
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-02-13 02:14:36 +0530

    wrote logic of the function includes isAnyWaitlisted, areAllConfirmed, getWaitlistedPassengers and 2 more

--- src/05-train-coach.js
+++ src/05-train-coach.js
@@ -49,20 +49,44 @@
  */
 export function findPassenger(passengers, name) {
   // Your code here
+
+  if(!Array.isArray(passengers) || typeof name !== "string") return undefined;
+
+  return passengers.find(passengersName => passengersName.name === name);
 }
 
 export function getPassengerIndex(passengers, name) {
   // Your code here
+
+  if(!Array.isArray(passengers) || typeof name !== "string") return -1;
+
+  return passengers.findIndex(nameIdx => nameIdx.name === name);
 }
 
 export function isAnyWaitlisted(passengers) {
   // Your code here
+
+  if(!Array.isArray(passengers) || passengers.length === 0) return false;
+  
+  return passengers.some(()=>{
+    passengers.status === "waitlisted";
+  });
 }
 
 export function areAllConfirmed(passengers) {
   // Your code here
+  if(!Array.isArray(passengers) || passengers.length === 0) return false;
+
+
+  return passengers.every(pass => pass.status === "confirmed");
 }
 
 export function getWaitlistedPassengers(passengers) {
   // Your code here
+
+  if(!Array.isArray(passengers)) return [];
+
+  return passengers.filter(()=>{
+    passengers.status === "waitlisted";
+  });
 }
